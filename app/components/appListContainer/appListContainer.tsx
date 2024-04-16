229af385333909d177b026496ac7f5f375fe030c
Phi Thai <thaimanhphi@gmail.com>
2024-04-17 00:06:12 +0700

    move the state up to home

--- app/components/appListContainer/appListContainer.tsx
+++ app/components/appListContainer/appListContainer.tsx
@@ -4,49 +4,16 @@ import styles from "./appListContainer.module.css";
 import { useEffect, useState } from "react";
 import { Application } from "@/app/model/Application";
 
-const AppListContainer = () => {
-  const [datas, setDatas] = useState<Application[]>([]);
-
-  const fetchData = async () => {
-    try {
-      const response = await getApplication();
-      setDatas(response.data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
-
-  const [enumData, setEnumData] = useState<Record<string, number> | null>(null);
-  async function createEnum() {
-    const enumData = await getStatus();
-    if (enumData) {
-      // Create an object with enum-like behavior
-      const Status = enumData.data.reduce(
-        (
-          acc: Record<string, number>,
-          { name, value }: { name: string; value: number }
-        ) => {
-          acc[name] = value;
-          return acc;
-        },
-        {} as Record<string, number>
-      );
-      setEnumData(Status);
-      return Status;
-    }
-    return null;
-  }
-
-  useEffect(() => {
-    fetchData();
-    createEnum();
-  }, []);
-
+interface AppListContainerProps {
+  datas: Application[];
+  enumData: Record<string, number> | null;
+}
+const AppListContainer: React.FC<AppListContainerProps> = (props) => {
   return (
     <div className={styles.list__container}>
       {/* render each  application in the list */}
-      {datas.map((data) => (
-        <AppList key={String(data._id)} enumStatus={enumData} {...data} />
+      {props.datas.map((data) => (
+        <AppList key={String(data._id)} enumStatus={props.enumData} {...data} />
       ))}
     </div>
   );
--- pages/home/HomePage.tsx
+++ pages/home/HomePage.tsx
@@ -3,10 +3,49 @@ import AddBtn from "@/app/components/client/addBtn";
 import styles from "./HomePage.module.css";
 import AppListContainer from "@/app/components/appListContainer/appListContainer";
 import AddCard from "@/app/components/addCard/addCard";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { Application } from "@/app/model/Application";
+import { getApplication, getStatus } from "@/app/networks/lib/home";
 
 const HomePage = () => {
   const [show, setShow] = useState(false);
+  const [enumData, setEnumData] = useState<Record<string, number> | null>(null);
+  const [datas, setDatas] = useState<Application[]>([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await getApplication();
+      setDatas(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  async function createEnum() {
+    const enumData = await getStatus();
+    if (enumData) {
+      // Create an object with enum-like behavior
+      const Status = enumData.data.reduce(
+        (
+          acc: Record<string, number>,
+          { name, value }: { name: string; value: number }
+        ) => {
+          acc[name] = value;
+          return acc;
+        },
+        {} as Record<string, number>
+      );
+      setEnumData(Status);
+      return Status;
+    }
+    return null;
+  }
+
+  useEffect(() => {
+    fetchData();
+    createEnum();
+  }, []);
+
   return (
     <>
       <div className={styles.body__container}>
@@ -14,7 +53,7 @@ const HomePage = () => {
         <div className={styles.add__btn}>
           <AddBtn enable={setShow} />
         </div>
-        <AppListContainer />
+        <AppListContainer datas={datas} enumData={enumData} />
       </div>
       {show && (
         <div className={`${styles.modal__container}`}>
